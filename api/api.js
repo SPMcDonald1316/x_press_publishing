ef06c682de6ae3e224efd85e8d21cd0d51770e80
Sean McDonald <spmcdonald1316@gmail.com>
2020-08-10 18:38:35 -0500

    create artists router and mount to api router

--- api/api.js
+++ api/api.js
@@ -1,4 +1,9 @@
+//Import express and other routers
 const express = require('express');
 const apiRouter = express.Router();
+const artistsRouter = require('./artists');
+
+//Mount different route paths
+apiRouter.use('/artists', artistsRouter);
 
 module.exports = apiRouter;
