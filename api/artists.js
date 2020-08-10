260aeba4bbfe7f2115bc5831b49ec6a6fdbb459c
Sean McDonald <spmcdonald1316@gmail.com>
2020-08-10 18:54:31 -0500

    code get handler for all artists

--- api/artists.js
+++ api/artists.js
@@ -1,4 +1,20 @@
+//Create Router and Import Database
 const express = require('express');
 const artistsRouter = express.Router();
+const sqlite3 = require('sqlite3');
+const db = new sqlite3.Database(process.env.TEST_DATABASE || './database.sqlite');
+
+//Routes
+artistsRouter.get('/', (req, res, next) => {
+  db.all('SELECT * FROM Artist WHERE is_currently_employed = 1', (err, artists) => {
+    if (err) {
+      //If it exists pass error along the middleware chain
+      next(err);
+    } else {
+      //Return all artists on the response object
+      res.status(200).json({artists: artists});
+    }
+  });
+});
 
 module.exports = artistsRouter;
