28525ee8f6120195c361ede3f030974c7704b4ca
Sean McDonald <spmcdonald1316@gmail.com>
2020-08-10 18:58:30 -0500

    code artist route parameter

--- api/artists.js
+++ api/artists.js
@@ -4,6 +4,20 @@ const artistsRouter = express.Router();
 const sqlite3 = require('sqlite3');
 const db = new sqlite3.Database(process.env.TEST_DATABASE || './database.sqlite');
 
+//Router param
+artistsRouter.param('artistId', (req, res, next, id) => {
+  db.get('SELECT * FROM Artist WHERE id = $id', {$id: id}, (err, artist) => {
+    if (err) {
+      next(err);
+    } else if (artist) {
+      req.artist = artist;
+      next();
+    } else {
+      res.sendStatus(404);
+    }
+  });
+});
+
 //Routes
 artistsRouter.get('/', (req, res, next) => {
   db.all('SELECT * FROM Artist WHERE is_currently_employed = 1', (err, artists) => {
