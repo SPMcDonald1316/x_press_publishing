1f7005d3a482eecff5f245aa614a96c5600f408c
Sean McDonald <spmcdonald1316@gmail.com>
2020-08-10 18:18:08 -0500

    create api router and mount

--- server.js
+++ server.js
@@ -1,15 +1,19 @@
-//Import packages
+//Import packages and modules
 const express = require('express');
 const morgan = require('morgan');
 const cors = require('cors');
 const errorHandler = require('errorhandler');
 const bodyParser = require('body-parser');
+const apiRouter = require('./api/api');
 
 //Create express app
 const app = express();
 
 //Set port
 const PORT = process.env.PORT || 4000;
+
+//Mount api router at /api
+app.use('/api', apiRouter);
 
 
 //Start server
