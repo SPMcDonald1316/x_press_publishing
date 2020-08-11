4ca796e5147997a4124692d07ac93241f2769897
Sean McDonald <spmcdonald1316@gmail.com>
2020-08-10 20:39:16 -0500

    create series router and mount to api router

--- api/api.js
+++ api/api.js
@@ -2,8 +2,10 @@
 const express = require('express');
 const apiRouter = express.Router();
 const artistsRouter = require('./artists');
+const seriesRouter = require('./series');
 
 //Mount different route paths
 apiRouter.use('/artists', artistsRouter);
+apiRouter.use('/series', seriesRouter);
 
 module.exports = apiRouter;
